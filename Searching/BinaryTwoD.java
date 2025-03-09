381cef92691bcb3a5d995f0db552e349eabc466a
DeluluSaro <saravanaiyyappan7@gmail.com>
2025-03-10 02:17:33 +0530

    Two d Arrays

---
+++ Searching/BinaryTwoD.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class BinaryTwoD {
+    public static int[] bsearch(int[][] matrix,int target){
+    int r=0;
+    int c=matrix[0].length-1;
+
+
+    while(r<matrix.length && c>=0){
+        if(matrix[r][c]==target){
+
+
+            return new int[]{r,c};
+
+        }
+        
+        if(matrix[r][c]>target){
+            c--;
+        }else{
+            r++;
+        }
+    }
+    
+        return new int[]{-1,-1};
+    }
+    public static void main(String[] args){
+        int[][] matrix = {
+            {1, 3, 5, 7},
+            {10, 11, 16, 20},
+            {23, 30, 34, 60}
+        };
+
+        int target=7;
+
+
+        System.out.println(Arrays.toString(bsearch(matrix,target)));
+        
+    }
+}
--- Searching/InfiniteArray.java
+++ Searching/InfiniteArray.java
@@ -21,11 +21,12 @@ public class InfiniteArray {
     }
     public static void main(String[] args) {
 
+        
 
         int start=0;
         int end=1;
-        int target=9;
-        int arr[]={1,2,3,4,5,6,7,8,9,10};
+        int target=15;
+        int arr[]={1,2,3,4,5,6,7,8,9,10,11,12,14,15};
 
 
 
